69606b6bc331dc91fa2c1cb445e10639606c863b
pgm-meesakveld <mees.akveld@student.arteveldehs.be>
2024-02-22 16:30:22 +0100

    Started on 11ty project

---
+++ .eleventy.js
@@ -0,0 +1,11 @@
+module.exports = function(eleventyConfig) {
+    
+    eleventyConfig.addPassthroughCopy("input/assets/");
+    
+    return {
+        dir: {
+            input: "./input",
+            output: "./output"
+        }
+    }
+};
