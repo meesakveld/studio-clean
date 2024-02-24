55b53c5704b010e6e2d605a3435a6d021d82cdfe
pgm-meesakveld <mees.akveld@student.arteveldehs.be>
2024-02-24 23:15:22 +0100

    Added limit filter

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig) {
     
     eleventyConfig.addPassthroughCopy("input/assets/");
+    eleventyConfig.addNunjucksFilter("limit", (arr, limit) => arr.slice(0, limit));
     
     return {
         dir: {
