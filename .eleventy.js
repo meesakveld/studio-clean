1e46739ee9a9fa2edb2269240903df02d18e2370
pgm-meesakveld <mees.akveld@student.arteveldehs.be>
2024-02-25 17:03:00 +0100

    Added custom date formatter

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,11 @@
+const { DateTime } = require("luxon");
+
 module.exports = function(eleventyConfig) {
     
     eleventyConfig.addNunjucksFilter("limit", (arr, limit) => arr.slice(0, limit));
+    eleventyConfig.addFilter("customDate", (dateObj) => {
+        return DateTime.fromJSDate(dateObj).toFormat("dd.MM.yyyy");
+    });
     eleventyConfig.addPassthroughCopy("input/assets/");
     
     return {
