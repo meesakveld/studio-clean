1a77e5d93af7194a436504325d0dbbd58314db12
pgm-meesakveld <mees.akveld@student.arteveldehs.be>
2024-02-29 21:31:19 +0100

    Added robots.txt to addPassThroughCopy in .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -15,7 +15,7 @@ module.exports = function(eleventyConfig) {
         return formattedDate;
     });
     eleventyConfig.addPassthroughCopy("input/assets/");
-    eleventyConfig.addPassthroughCopy("robots.txt");
+    eleventyConfig.addPassthroughCopy("input/robots.txt");
     
     return {
         dir: {
