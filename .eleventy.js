7d647651ed125f4602ee4337ae087c0f7def2cb9
pgm-meesakveld <mees.akveld@student.arteveldehs.be>
2024-02-29 08:57:56 +0100

    Added limit and sortByOrder filters to eleventyConfig

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,15 @@ const { DateTime } = require("luxon");
 
 module.exports = function(eleventyConfig) {
     
-    eleventyConfig.addNunjucksFilter("limit", (arr, limit) => arr.slice(0, limit));
+    eleventyConfig.addNunjucksFilter("limit", (arr, limit) => {
+        if (!limit) return arr;
+        return arr.slice(0, limit);
+    });
+
+    eleventyConfig.addFilter("sortByOrder", (navItem) => {
+        return navItem.sort((a, b) => a.data.order - b.data.order);
+    });
+
     eleventyConfig.addFilter("customDate", (dateObj) => {
         return DateTime.fromJSDate(dateObj).toFormat("dd.MM.yyyy");
     });
