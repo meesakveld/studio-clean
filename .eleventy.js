bb96f6cd270f5261dd747351ee3584ff7973231e
meesakveld <meesakveld@gmail.com>
2025-01-15 18:57:49 +0100

    Updated output directory to './docs' and added GitHub Actions workflow for deployment

--- .eleventy.js
+++ .eleventy.js
@@ -19,7 +19,7 @@ module.exports = function (eleventyConfig) {
     return {
         dir: {
             input: "./input",
-            output: "./output"
+            output: "./docs"
         }
     }
 };
